37e1ba23d2447563c4e8d06bcddbcf9896e527b8
MetaTrack <rodrigometa4800@gmail.com>
2026-04-18 02:24:20 -0500

    Fix 19

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER – Coach Finanzas PWA
 // =============================================
 
-const CACHE_NAME = 'finanzas-v18';
+const CACHE_NAME = 'finanzas-v19';
 
 const ASSETS = [
   '/App-Finanzas/',
